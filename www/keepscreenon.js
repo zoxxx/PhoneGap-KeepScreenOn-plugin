d6cf868bb99f493037dcf69c28087b02aac4718c
Zoran B <zoranb007@yahoo.com>
2014-04-15 22:35:16 +0200

    js export fix

--- www/keepscreenon.js
+++ www/keepscreenon.js
@@ -1,34 +1,32 @@
-var keepScreenOn = {
+function keepScreenOn () {
+	this.screenOnFlag = false;
+};
 
-	// Set a global screen locked variable
-	screenOnFlag: false,
+/*
+* Acquire a screenlock (prevent the device from screen idle)
+*/
+keepScreenOn.prototype.enable = function () {
+	this.screenOnFlag = true;
+	cordova.exec(null, null, 'KeepScreenOn', 'enable', []);
+};
 
-	/*
-	* Acquire a screenlock (prevent the device from screen idle)
-	*/
-	enable: function()
-	{
-		cordova.exec(null, null, 'KeepScreenOn', 'enable', []);
-		keepScreenOn.screenOnFlag = true;
-	},
-
-	/*
-	* Release the screenlock (enable the device screen to idle)
-	*/
-	disable: function()
-	{
-		cordova.exec(null, null, 'KeepScreenOn', 'disable', []);
-		keepScreenOn.screenOnFlag = false;
-	},
-
-	/*
-	*	Toggle the screenlock
-	*/
-	toggleKeepScreenOn: function() {
-		if(this.screenOnFlag) {
-			this.disable();
-		} else {
-			this.enable();
-		}
-	}
+/*
+* Release the screenlock (enable the device screen to idle)
+*/
+keepScreenOn.prototype.disable = function () {
+	this.screenOnFlag = false;
+	cordova.exec(null, null, 'KeepScreenOn', 'disable', []);
 }
+
+/*
+*	Toggle the screenlock
+*/
+keepScreenOn.prototype.toggle = function () {
+	if(this.screenOnFlag) {
+		this.disable();
+	} else {
+		this.enable();
+	}
+};
+
+module.exports = new keepScreenOn();
